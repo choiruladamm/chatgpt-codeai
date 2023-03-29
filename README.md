47affde49262a896b4b172cb839cc58c9b75e907
choiruladamm <choiruladamm@gmail.com>
2023-03-29 14:30:35 +0700

    Code AI

---
+++ README.md
@@ -0,0 +1,1 @@
+# ChatGPT - Code AI
---
+++ client/script.js
@@ -0,0 +1,37 @@
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
+
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = "";
+
+  loadInterval = setInterval(() => {
+    element.textContent += ".";
+
+    if (element.textContent === "....") {
+      element.textContent = "";
+    }
+  }, 300);
+}
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.chartAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20);
+}
+
+function generateUniqueId() {
+  const timeStamp = Date.now();
+  const randomNumber = Math.random();
+}
