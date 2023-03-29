8bbb1e75c4833cab40835f54459fadeeb6d091a3
choiruladamm <choiruladamm@gmail.com>
2023-03-29 14:58:14 +0700

    Code AI

--- client/script.js
+++ client/script.js
@@ -34,4 +34,51 @@ function typeText(element, text) {
 function generateUniqueId() {
   const timeStamp = Date.now();
   const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timeStamp}-${hexadecimalString}`;
 }
+
+function chatStripe(isAi, value, uniqueId) {
+  return `
+      <div class="wrapper ${isAi && "ai"}">
+        <div class="chat">
+          <div class="profile">
+            <img 
+              src=${isAi ? bot : user}
+              alt=${isAi ? "bot" : "user"}
+            />
+          </div>
+          <div class="message" id=${uniqueId}>${value}</div>
+        </div>
+      </div>
+    `;
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+
+  form.reset();
+
+  // bot's chatstripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  loader(messageDiv);
+};
+
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
