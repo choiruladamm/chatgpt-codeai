b95bfdc380e63aa6863be288b2e9005ff915ddde
choiruladamm <choiruladamm@gmail.com>
2023-04-13 22:22:44 +0700

    Fix Deployment

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch("https://server-codeai.cyclic.app/", {
+  const response = await fetch("https://openai-clone.cyclic.app/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
